e7cce235b7ba6635d5fd3fdaf2dfae967387385d
Sam Lee <cable8mm@gmail.com>
2022-12-04 23:17:37 +0900

    npm run deploy 명령어 추가

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,1 +1,1 @@
-rsync -arvuz --exclude-from='.rsyncignore' /Users/cable8mm/Sites/maintenance.seoultanzstation/ forge@52.79.57.223:/home/forge/seoultanzstation.com/storage/maintenance
+rsync -arvuz --exclude-from='.rsyncignore' ~/Sites/maintenance.seoultanzstation/ forge@52.79.57.223:/home/forge/seoultanzstation.com/storage/maintenance
